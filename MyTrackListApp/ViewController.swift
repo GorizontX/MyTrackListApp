637ec9ad71eeebee6b51afbd322345466115138a
Andrey <65368541+GorizontX@users.noreply.github.com>
2023-04-10 01:08:01 +0200

    Initial Commit

---
+++ MyTrackListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyTrackListApp
+//
+//  Created by Andrey Machulin on 10.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
